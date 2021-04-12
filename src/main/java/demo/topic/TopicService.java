ed95eef3c123ce2d1f7cc1e0f06926070baa79dc
anas zabili <anaszab78@gmail.com>
2021-04-12 18:21:55 +0200

    lesson 19

--- src/main/java/demo/topic/TopicService.java
+++ src/main/java/demo/topic/TopicService.java
@@ -1,5 +1,6 @@
 package demo.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,11 +8,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-  private List<Topic> topics = Arrays.asList(new Topic("1", "Javascript courses", "un cour super"),
-      new Topic("2", "Java", "un cour super sur java"));
+  private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("1", "Javascript courses", "un cour super"),
+      new Topic("2", "Java", "un cour super sur java")));
 
   public List<Topic> getAllTopics() {
     return topics;
   }
 
+  public Topic getTopic(String id) {
+    return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+  }
+
+  public void addTopic(Topic topic) {
+    topics.add(topic);
+  }
 }
